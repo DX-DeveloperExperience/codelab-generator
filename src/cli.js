8de0c0de98dffde244013316cfef36c60cb0f7e9
Aurélien Loyer <aur.loy@gmail.com>
2020-10-19 23:58:12 +0200

    feat: accept configuration file ⚙️

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 
+const path = require("path");
 const [, , ...args] = process.argv;
+let config = {};
 
-if (args.length !== 2) {
-  console.error("You should define two arguments to this command line");
+if (args.length < 2) {
+  console.error(
+    "You should define a minimum of two arguments to this command line"
+  );
   process.exit();
 }
 
-const path = require("path");
+if (args.length === 3) {
+  const configPath = path.resolve(process.cwd(), args[3]);
+
+  try {
+    config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+}
+
 const input = path.resolve(process.cwd(), args[0]);
 const output = path.resolve(process.cwd(), args[1]);
 
-require("./index")(input, output);
+require("./index")(input, output, config);
